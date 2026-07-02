fdae68500aaef574ad50c5e0a0dd14a6ba9c0c11
ShynaaC <shynaa.chopra@gmail.com>
2026-07-03 01:17:33 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/companyintranet/CompanyintranetApplication.java
@@ -0,0 +1,13 @@
+package com.example.companyintranet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyintranetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyintranetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/companyintranet/CompanyintranetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companyintranet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyintranetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
